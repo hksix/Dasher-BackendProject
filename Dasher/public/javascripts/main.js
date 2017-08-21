f76a5f047126955fb4fddb253583271dd189d15a
jenlij <jljohns1216@gmail.com>
2017-08-21 16:00:43 -0400

    adds weather and date

--- Dasher/public/javascripts/main.js
+++ Dasher/public/javascripts/main.js
@@ -7,23 +7,61 @@ $(function(){
 
 //add clock in widget1 locatiojn
 function insertClock(){
-    $('.widget1').html('<div class="clockwidget"></div>');
+    $('.widget1').html('<div class="clockWidget"></div>');
     // FlipClock initialization  
     var ServerTime = $('#my_time').val();
     var d = new Date();
     var n = d.getHours();
     var diff = ServerTime - n;
-    clockwidget = $('.clockwidget').FlipClock(diff,{  
+    $('.clockWidget').FlipClock(diff,{  
         clockFace: 'TwentyFourHourClock',
         showSeconds: false  
     });  
 }
 insertClock();
 
-function insertWeather(){
-
+function insertWeather(city, units){
+    $('.widget3').html('<div class="weatherWidget"></div>')
+    $(document).ready(function() {
+        getWeather(); //Get the initial weather.
+        setInterval(getWeather, 600000); //refresh every 10 minutes 
+    });
+        function getWeather(){
+            $.simpleWeather({
+                location: city,
+                woeid: '',
+                unit: units,
+                success: function(weather) {
+                var html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+                html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+                html += '<li class="currently">'+weather.currently+'</li>';
+                html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+                $('.weatherWidget').html(html)
+          },
+            error: function(error) {
+                $('.weatherWidget').html(error)
+            }
+        });
+    }
 }
 
+insertWeather('Atlanta, GA', 'f');
+
+function insertDate(){
+    $('.widget2').html('<div class="dateWidget"></div>');
+    var d = new Date();
+    var dayOfWeek = d.getDay();
+    var dayOfMonth = d.getDate();
+    var month = d.getMonth();
+    var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+    'August', 'September', 'October', 'November', 'December'];
+    var year = d.getFullYear();
+    $('.dateWidget').html(
+        '<div class="DOW">'+ dayNames[dayOfWeek] + '</div>'+
+        '<div class "DateString">'+ monthNames[month] + ' ' + dayOfMonth + ' '+ year +'</div>');
+}
+insertDate();
 
 
 // function clock(elt) {
@@ -58,37 +96,40 @@ function insertWeather(){
 //       startTime();  
 // }
 
-function weather(elt){
-    console.log('weather');
-    // let div = elt.parentElement.previousSibling.previousSibling.previousSibling.nextSibling; 
-    let div = elt.parentElement.previousSibling.previousSibling;
-    console.log(div);
-    elt.addEventListener("click", function(){
-        // div.style.display = "block";
-    });
-    $(document).ready(function() {
-        getWeather(); //Get the initial weather.
-        setInterval(getWeather, 600000);  
-    });
-        function getWeather(){
-            $.simpleWeather({
-          location: 'Atlanta, GA',
-          woeid: '',
-          unit: 'f',
-          success: function(weather) {
-            var html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-            html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-            html += '<li class="currently">'+weather.currently+'</li>';
-            html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-            div.innerHTML = html;
-            console.log(html);
-          },
-          error: function(error) {
-            div.innerHTML = '<p>'+error+'</p>';
-          }
-        });
-    }
-}
+// function weather(elt){
+//     console.log('weather');
+//     // let div = elt.parentElement.previousSibling.previousSibling.previousSibling.nextSibling; 
+//     let div = elt.parentElement.previousSibling.previousSibling;
+//     console.log(div);
+//     elt.addEventListener("click", function(){
+//         // div.style.display = "block";
+//     });
+//     $(document).ready(function() {
+//         getWeather(); //Get the initial weather.
+//         setInterval(getWeather, 600000);  
+//     });
+//         function getWeather(){
+//             $.simpleWeather({
+//           location: 'Atlanta, GA',
+//           woeid: '',
+//           unit: 'f',
+//           success: function(weather) {
+//             var html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+//             html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+//             html += '<li class="currently">'+weather.currently+'</li>';
+//             html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+//             div.innerHTML = html;
+//             console.log(html);
+//           },
+//           error: function(error) {
+//             div.innerHTML = '<p>'+error+'</p>';
+//           }
+//         });
+//     }
+// }
+
+
+
 function rss(elt){
     let div = elt.parentElement.parentElement.parentElement.parentElement;
     console.log(div);
@@ -112,5 +153,4 @@ function rss(elt){
 }
 
 
-//need a module.exports here so we can require these functions withn dashboard.js
 

e02c0f6edfbd199b5c5fb31c5b1769701410134f
Stephanie Asmar <sasmar8@gmail.com>
2017-08-21 22:20:16 -0400

    about page route

---
+++ Dasher/routes/about.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET about page */
+router.get('/, function(req, res, next) {
+    res.render('about',{
+        title: 'Dasher | Welcome', 
+        layout:'layout'
+    });
+});
+
+
+module.exports = router;
